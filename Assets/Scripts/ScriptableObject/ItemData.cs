954090bd519e2734d777b9983e667ed6af7f4e97
SOSJiwon <aki_0113@naver.com>
2024-06-05 22:38:10 +0900

    Add : All of Item
    UI 폰트 변경 - NotoSans KR
    UI 일부분 폰트 사이즈 변경

--- Assets/Scripts/ScriptableObject/ItemData.cs
+++ Assets/Scripts/ScriptableObject/ItemData.cs
@@ -3,42 +3,42 @@ using UnityEngine;
 
 public enum ItemType
 {
-	Equipable,
-	Consumable,
-	Resource
+	Equipable, //장비
+	Consumable, //소모품
+	Resource //자원
 }
 
 public enum ConsumableType
 {
-	Health,
-	Hunger
+	Health, //체력
+	Hunger //허기
 }
 
 [Serializable]
 public class ItemDataConsumable
 {
-	public ConsumableType type;
-	public float value;
+	public ConsumableType type; //회복 타입
+	public float value; //회복 값
 }
 
 [CreateAssetMenu(fileName = "Item", menuName = "New Item")]
 public class ItemData : ScriptableObject
 {
 	[Header("Info")]
-	public string displayName;
-	public string description;
+	public string displayName; // 디스플레이에 뜨는 이름
+	public string description; // 설명
 	public ItemType type;
 	public Sprite icon;
-	public GameObject dropProefab;
+	public GameObject dropProefab; 
 
 	[Header("Stacking")]
-	public bool canStack;
-	public int maxStackAmount;
+	public bool canStack; //중복 소유가 가능한가?
+	public int maxStackAmount; //한번에 몇개나 지닐 수 있는가
 
 	[Header("Consumable")]
-	public ItemDataConsumable[] consumables;
+	public ItemDataConsumable[] consumables; //소모품일 경우 배열로 체력, 배고픔
 
 	[Header("Equip")]
-	public GameObject equipPrefab;
+	public GameObject equipPrefab; //장비 프리팹
 
 }
