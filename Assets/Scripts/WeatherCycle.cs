03893752b80a14612e580f92aee55aab4bdc391c
Kaldorei00910 <117884183+Kaldorei00910@users.noreply.github.com>
2024-06-09 14:28:26 +0900

    feature / WeatherCycle
    이제 자연스럽게 날씨가 변경됩니다

--- Assets/Scripts/WeatherCycle.cs
+++ Assets/Scripts/WeatherCycle.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum weatherState
+{
+    SUN,
+    RAIN,
+    SNOW
+}
 
 public class WeatherCycle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject rain;
+    public GameObject snow;
+    public float durationTime;
+
+    private float nowtime = 0;
+    private int weathernum = 0;
+
+    private void Start()
     {
-        
+        WeatherDisable(rain);
+        WeatherDisable(snow);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        nowtime += Time.deltaTime;
+
+        if(nowtime > durationTime)
+        {
+            weathernum = Random.Range(0, 2);
+
+            DuringWeather(weathernum);
+
+            nowtime = 0;
+        }
     }
+
+    private void DuringWeather(int weathernum)
+    {
+        switch (weathernum)
+        {
+            case (int)weatherState.SUN:
+                WeatherDisable(rain);
+                WeatherDisable(snow);
+                break;
+            case (int)weatherState.RAIN:
+                WeatherEnable(rain);
+                WeatherDisable(snow);
+                break;
+            case (int)weatherState.SNOW:
+                WeatherEnable(snow);
+                WeatherDisable(rain);
+                break;
+        }
+
+    }
+
+    private void WeatherEnable(GameObject Weather)
+    {
+        ParticleSystem weatherParticleSystem = new ParticleSystem();
+        weatherParticleSystem = Weather.GetComponent<ParticleSystem>();
+        weatherParticleSystem.Play();
+    }
+
+    private void WeatherDisable(GameObject Weather)
+    {
+        ParticleSystem weatherParticleSystem = new ParticleSystem();
+        weatherParticleSystem = Weather.GetComponent<ParticleSystem>();
+        weatherParticleSystem.Stop();
+    }
+
 }
